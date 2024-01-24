6cbb7971a12965a684a843a43775dc41471c933b
TheJeth <145587508+TheJeth@users.noreply.github.com>
2024-01-24 04:11:34 +0000

    First commit

--- src/models.py
+++ src/models.py
@@ -1,29 +1,73 @@
 import os
 import sys
-from sqlalchemy import Column, ForeignKey, Integer, String
+from sqlalchemy import Column, ForeignKey, Integer, String, LargeBinary
 from sqlalchemy.orm import relationship, declarative_base
 from sqlalchemy import create_engine
 from eralchemy2 import render_er
 
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ = 'person'
+class User(Base):
+    __tablename__ = 'user'
     # Here we define columns for the table person
     # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    name = Column(String(250), nullable=False)
+    user_id = Column(Integer, primary_key=True)
+    user_name = Column(String(50), nullable=False)
+    user_email = Column(String(30), nullable=False)
+    user_tel = Column(String(13), nullable=False)
+    user_privilege = Column(String(50), nullable=False)
 
-class Address(Base):
-    __tablename__ = 'address'
+class Admin(Base):
+    __tablename__ = 'admin'
+    # Here we define columns for the table person
+    # Notice that each column is also a normal Python instance attribute.
+    admin_id = Column(Integer, primary_key=True)
+    admin_name = Column(String(50), nullable=False)
+    admin_email = Column(String(30), nullable=False)
+    admin_tel = Column(String(13), nullable=False)
+    admin_privilege = Column(String(50), nullable=False)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+
+
+class Post(Base):
+    __tablename__ = 'post'
     # Here we define columns for the table address.
     # Notice that each column is also a normal Python instance attribute.
-    id = Column(Integer, primary_key=True)
-    street_name = Column(String(250))
-    street_number = Column(String(250))
-    post_code = Column(String(250), nullable=False)
-    person_id = Column(Integer, ForeignKey('person.id'))
-    person = relationship(Person)
+    post_id = Column(Integer, primary_key=True)
+    post_title = Column(String(250), nullable=False)
+    post_text = Column(String(10250), nullable=False)
+    post_photo = Column(LargeBinary)
+    post_like = Column(Integer)
+    post_comment = Column(String(500))
+    post_favorite = Column(Integer)
+    user_id = Column(Integer, ForeignKey('user.id'))
+    user = relationship(User)
+
+class WritePost(Base):
+    __tablename__ = 'writepost'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    user_id = Column(Integer, primary_key=True)
+    post_id = Column(Integer, primary_key=True)
+    user = relationship(User)    
+    user = relationship(Post)
+
+class SavePost(Base):
+    __tablename__ = 'savepost'
+    # Here we define columns for the table address.
+    # Notice that each column is also a normal Python instance attribute.
+    user_id = Column(Integer, primary_key=True)
+    post_id = Column(Integer, primary_key=True)
+    post_title = Column(String(250), nullable=False)
+    post_text = Column(String(10250), nullable=False)
+    post_photo = Column(LargeBinary)
+    post_like = Column(Integer)
+    post_comment = Column(String(500))
+    post_favorite = Column(Integer)
+    user = relationship(User)    
+    user = relationship(Post)
 
     def to_dict(self):
         return {}
